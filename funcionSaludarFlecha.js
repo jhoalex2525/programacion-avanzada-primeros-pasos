27c032c7d412b1698482c41c6a62cc923d105362
jhonatan.ossa <jhonatan.ossa@ceiba.com.co>
2022-02-18 07:59:47 -0500

    Se agrega funcion flecha

---
+++ funcionSaludarFlecha.js
@@ -0,0 +1,10 @@
+//  funcion tradicional
+function saludarTradicional(nombrePersona){
+    return ("Buenos días "+nombrePersona)
+}
+
+console.log(saludarTradicional("Jhony"))
+
+// funcion de flecha
+
+let saludar =
