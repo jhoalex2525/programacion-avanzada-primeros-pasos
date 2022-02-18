a1805fb87ff44c877b774c8a27dded67eaeb0006
jhonatan.ossa <jhonatan.ossa@ceiba.com.co>
2022-02-18 08:09:56 -0500

    Se agrega funcion flecha sumar

--- funcionSaludarFlecha.js
+++ funcionSaludarFlecha.js
@@ -5,6 +5,13 @@ function saludarTradicional(nombrePersona){
 
 console.log(saludarTradicional("Jhony"))
 
-// funcion de flecha
+// funcion de flecha, si solo hay un parametro, s epueden quitar lso parentesis del parametro
+// y si ocupa el codigo una sola linea se puede quitar el return, quedaria:
+// let saludar = nombrePersona => "Buenos días "+nombrePersona
+// console.log(saludar("Juan")) 
 
-let saludar =
+let saludar = (nombrePersona) => {
+    return ("Buenos días "+nombrePersona)
+}
+
+console.log(saludar("Juan"))
