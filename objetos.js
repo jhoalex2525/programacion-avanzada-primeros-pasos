fcb5898e699155f7e83a7632284864e9fcc6b647
jhonatan.ossa <jhonatan.ossa@ceiba.com.co>
2022-02-25 07:55:56 -0500

    Ser termina el objeto

---
+++ objetos.js
@@ -0,0 +1,16 @@
+// objetos (explicacion con morrales/bolsillos)
+// sirven para para ordenar info de diferente tipo en una sola variable
+//atributos: valor/dato
+
+// objeto
+let persona = {
+    nombre: "Jhony Ossa",
+    telefono: 123456789,
+    esPlayer: true
+}
+
+// imprimir objeto compelto
+console.log(persona)
+
+// impirmir un solo atributo
+console.log(persona.nombre)
