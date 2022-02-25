af013b2c0e8159ec85e386b1c716bdad6dd69e90
jhonatan.ossa <jhonatan.ossa@ceiba.com.co>
2022-02-25 07:24:22 -0500

    se termina explicacion de arreglos e iniciamos con objetos

---
+++ arreglos.js
@@ -0,0 +1,13 @@
+// arreglos de datos en js
+// son "cajas" para almacenar DATOS
+// 1 unica variable almacena varios datos
+// ORDEN
+
+// mostrando arreglos del mismo tipo
+let nombres = ["juan","jhony","maria","cristian","rosa"]
+let edades = [10,15,16,20,30,60]
+let promociones = [true,false,false,true]
+// abriendo todas las cajas
+console.log(nombres)
+// abriendo una sola caja
+console.log(nombres[3])
